7f7be27de48e0d8c18ef6f12e879ced46b3b8d4d
EduardoDamSilva <eduardo.silva@sptech.school>
2023-09-26 18:21:30 -0300

    exercicios map

---
+++ map/array.js
@@ -0,0 +1,40 @@
+// 1
+const arr = [1, 2, 3, 4, 5, 6]
+
+// 2
+const usuarios = [
+  {
+    nome: "Eduardo",
+    idade: 22
+  },
+  {
+    nome: "João",
+    idade: 30,
+  },
+  {
+    nome: "Claudio",
+    idade: 40
+  }
+]
+
+
+// 3
+const vendas = [
+  {
+    produto: "Estojo",
+    preco: 50.00,
+    qtdVendidas: 10
+  },
+  {
+    produto: "Lapis",
+    preco: 2.00,
+    qtdVendidas: 20
+  },
+  {
+    nome: "Branquinho",
+    preco: 7.00,
+    qtdVendidas: 15
+  }
+]
+
+// Saída: O Estojo vendeu 10 unidades a R$ 50.00 que no total fica R$total
